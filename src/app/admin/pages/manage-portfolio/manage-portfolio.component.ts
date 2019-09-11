d712746163ff14293c7d730ef75889271da92870
Sebastian Gug <sebastiangug@gmail.com>
2019-09-11 19:58:05 +0100

    Initial files

---
+++ src/app/admin/pages/manage-portfolio/manage-portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-portfolio',
+  templateUrl: './manage-portfolio.component.html',
+  styleUrls: ['./manage-portfolio.component.scss']
+})
+export class ManagePortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    ContactComponent,
+    AboutComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
