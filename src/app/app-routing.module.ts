35275725a533e1fddbe9a1cc92e2ae35eff0b5c6
Sebastian Gug <sebastiangug@gmail.com>
2020-03-05 16:28:00 +0000

    code interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { SkillsComponent } from './pages/skills/skills.component';
-import { PortfolioComponent } from './pages/portfolio/portfolio.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { AdminGuard } from './guards/admin.guard';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { LoginComponent } from './pages/login/login.component';
-import { TagComponent } from './pages/tag/tag.component';
+import { CodeGuard } from './guards/code.guard';
+import { CodeComponent } from './pages/code/code.component';
 
 const routes: Routes = [
   {
     path: 'public',
     loadChildren: () =>
       import('./modules/main/main.module').then(m => m.MainModule),
+    canLoad: [CodeGuard],
   },
-  // { path: 'skills', component: SkillsComponent, pathMatch: 'full' },
-  // { path: 'portfolio', component: PortfolioComponent, pathMatch: 'full' },
-  // {
-  //   path: 'about',
-  //   component: AboutComponent,
-  //   pathMatch: 'full',
-  // },
-  // {
-  //   path: 'contact',
-  //   component: ContactComponent,
-  //   pathMatch: 'full',
-  // },
+  {
+    path: 'code',
+    component: CodeComponent,
+    pathMatch: 'full',
+  },
   {
     path: 'admin',
     loadChildren: () =>
       import('./modules/admin/admin.module').then(m => m.AdminModule),
     canLoad: [AdminGuard],
   },
-  // {
-  //   path: 'tag/:tag',
-  //   component: TagComponent,
-  // },
-
   { path: '404', component: NotFoundComponent },
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', component: CodeComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '404' },
 ];
 
--- src/app/pages/code/code.component.ts
+++ src/app/pages/code/code.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-code',
   templateUrl: './code.component.html',
-  styleUrls: ['./code.component.scss']
+  styleUrls: ['./code.component.scss'],
 })
 export class CodeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  public accessMain(code: string) {}
 }
